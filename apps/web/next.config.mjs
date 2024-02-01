e3843e2b2a4f4aa840cda47fc199c17fe8672bbc
AMR-21 <amryasser52001@gmail.com>
2024-02-02 01:13:37 +0200

    Fixed a type error and generalized the tsconfig + First steps of adding auth.js

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,3 +1,5 @@
+import { setupDevBindings } from "@cloudflare/next-on-pages/next-dev";
+
 await import("./lib/env.mjs");
 
 /** @type {import('next').NextConfig} */
@@ -10,9 +12,7 @@ const nextConfig = {
  */
 
 if (process.env.NODE_ENV === "development") {
-  const { setupDevBindings } = require("@cloudflare/next-on-pages/next-dev");
-
-  setupDevBindings({
+  await setupDevBindings({
     bindings: {
       DB: {
         type: "d1",
