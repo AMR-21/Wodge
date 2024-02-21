bed0840bbb5e581fea4d343be15d92c221ca3f2b
AMR-21 <amryasser52001@gmail.com>
2024-02-21 02:25:42 +0200

    Added env package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 // This configuration only applies to the package manager root.
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  ignorePatterns: ["apps/**", "core/**", "config/**"],
+  ignorePatterns: ["apps/**", "packages/**", "config/**"],
   extends: ["turbo"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,61 +1,10 @@
+//@ts-nocheck
 import { setupDevBindings } from "@cloudflare/next-on-pages/next-dev";
-await import("./src/lib/env.js");
-
-// const headers = [
-//   "Accept",
-//   "Accept-Version",
-//   "Content-Length",
-//   "Content-MD5",
-//   "Content-Type",
-//   "Date",
-//   "X-Api-Version",
-//   "X-CSRF-Token",
-//   "X-Requested-With",
-// ];
+await import("./node_modules/@repo/env/index.js");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@radix-ui"],
-  // env: {
-  //   ALLOWED_NEXT_AUTH_URLS: [
-  //     "http://(.+\\.|)localhost:3000/?",
-  //     "http://(.+\\.|)localhost:1999/?",
-  //   ],
-  //   ALLOWED_HEADERS: headers.join(", "),
-  //   CORS_DEFAULTS: {
-  //     methods: [], // making this blank by default - you have to override it per-call
-  //     origin: "*",
-  //     allowedHeaders: headers.join(", "),
-  //     credentials: true,
-  //   },
-  // },
-  // async headers() {
-  //   return [
-  //     {
-  //       source: "/api/(.*)",
-  //       headers: [
-  //         { key: "Access-Control-Allow-Credentials", value: "true" },
-  //         { key: "Access-Control-Allow-Origin", value: "*" },
-  //         {
-  //           key: "Access-Control-Allow-Methods",
-  //           value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-  //         },
-  //         {
-  //           key: "Access-Control-Allow-Headers",
-  //           value: headers.join(", "),
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
-  // rewrites: async () => [
-  //   {
-  //     // forward room authentication request to partykit
-  //     source: "/me",
-  //     // include connection id in the query
-  //     destination: "https://backend.amr-21.partykit.dev" + "/parties/main/id",
-  //   },
-  // ],
 };
 
 /**
