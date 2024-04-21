562810a944041ba924624e75e1ff74cc2ebd2504
AMR-21 <amryasser52001@gmail.com>
2024-04-21 22:34:38 +0200

    removal of next auth

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -14,29 +14,29 @@ export type Mutable<Type> = {
 /**
  * A function that sends email magic link to the user using Resend api.
  */
-export async function sendMagicLink({
-  identifier: email,
-  url,
-}: VerificationMail) {
-  try {
-    await fetch("https://api.resend.com/emails", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${env.RESEND_API_KEY}`,
-      },
-      body: JSON.stringify({
-        to: [email],
-        from: env.RESEND_EMAIL_FROM,
-        subject: "Verify your email",
-        html: `<a href="${url}">Verify your email ${url}</a>`,
-      }),
-    });
-  } catch (error) {
-    console.error("Error sending email:", error);
-    // throw new Error("Error sending email");
-  }
-}
+// export async function sendMagicLink({
+//   identifier: email,
+//   url,
+// }: VerificationMail) {
+//   try {
+//     await fetch("https://api.resend.com/emails", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `Bearer ${env.RESEND_API_KEY}`,
+//       },
+//       body: JSON.stringify({
+//         to: [email],
+//         from: env.RESEND_EMAIL_FROM,
+//         subject: "Verify your email",
+//         html: `<a href="${url}">Verify your email ${url}</a>`,
+//       }),
+//     });
+//   } catch (error) {
+//     console.error("Error sending email:", error);
+//     // throw new Error("Error sending email");
+//   }
+// }
 
 export async function currentUser() {
   // const session = await auth();
